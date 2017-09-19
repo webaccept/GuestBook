4c9a293875bb392b59d216a96282cff92821592a
Andrey Shtytsko <webaccept@mail.ru>
2017-09-19 16:09:07 +0400

    # test

--- index.php
+++ index.php
@@ -3,7 +3,10 @@ define('PATH', $_SERVER['DOCUMENT_ROOT']);
 define('GB', PATH.'/upload/gb.xml');
 
 spl_autoload_register(function ($class) {
-    include PATH.'/clss/'.$class.'.php';
+    $path = PATH . '/clss/' . $class . '.php';
+    if (file_exists($path)) {
+        include $path;
+    }
 });
 
 $obj = new ClassMain();
@@ -12,6 +15,6 @@ if (!empty($_POST)) {
     $obj->ajaxSet($_POST);
 }
 
+// проверка отправки git
 $obj->IncludeTemplate('index', null);
-
 ?>
